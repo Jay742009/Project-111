f22d0d8a87a3650b35e9dea19dff358702017250
Jay742009 <72810212+Jay742009@users.noreply.github.com>
2021-04-27 13:10:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+Webcam.set({
+    height: 300,
+    width: 350,
+    image_format: 'png',
+    png_quality: 90,
+
+    constraints: {
+        facingMode: "envoirnment"
+    }
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takesnapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("results").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('MobileNet', modelloaded);
+
+function modelloaded() {
+    console.log('modelloaded');
+}
+
+function identify_object() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotresult);
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
